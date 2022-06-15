9c44a99bef3a1a3690430d38979d33c815da3294
Alexandros Lagkonas <lagkonas26@gmail.com>
2022-06-15 17:09:37 +0300

    Production

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import { ReactQueryDevtools } from 'react-query/devtools';
+// import { ReactQueryDevtools } from 'react-query/devtools';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Home from './pages/Home';
